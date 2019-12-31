372b0a7bdb9573dffef9bb77efa4d41f3d5723ce
Abraham <dogcalas@gmail.com>
2019-12-31 02:47:39 -0500

    bd fixed

--- telegramBot.js
+++ telegramBot.js
@@ -7,6 +7,15 @@ const fs = require('fs')
 const Axios = require('axios')
 const parsePodcast = require('node-podcast-parser');
 const Lame = require("node-lame").Lame;
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('dbbot.json')
+const db = low(adapter)
+
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ podcasts: [] })
+    .write()
 
 
 const fileExists = async path => !!(await fs.promises.stat(path).catch(e => false));
@@ -87,7 +96,7 @@ async function compressAudio(chapter) {
                 encoder
                     .encode()
                     .then(() => {
-                       resolve(path)
+                        resolve(path)
                     })
                     .catch(error => {
                         reject('Ocurrio un error al comprimir')
@@ -101,26 +110,70 @@ async function compressAudio(chapter) {
 
 }
 
-
-const findchapter = async (msg, props) => {
+const getAudio = async (msg, props, full) => {
     try {
         const chapter = props.match[1];
-        if(!isNaN(chapter)){
-        // bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
-        const file = await downloadAudio(chapter)
-        const compressed = await compressAudio(chapter)
-        console.log(file, 'DOWNLOADED')
-        bot.sendAudio(msg.from.id, compressed, { replyToMessage: msg.message_id, title: `Episodio ${chapter} - El enjambre` })
-    } else {
-        bot.sendMessage(msg.from.id, 'Debe escribir solo el número del episodio.', { replyToMessage: msg.message_id });   
-    }
+
+        //SAVE STATS
+        const podcast = db.get('podcasts')
+            .find({ name: `Episodio ${chapter}` })
+            .value()
+
+        if (!podcast) {
+            db
+                .get('podcasts')
+                .push({ name: `Episodio ${chapter}`, countfull: (full)?1:0, countcomp:(full)?0:1 })
+                .write()
+        } else {
+            db.get('podcasts')
+                .find({ name: `Episodio ${chapter}` })
+                .update((full)?'countfull':'countcomp', n => n + 1)
+                .write()
+        }
+
+
+        if (!isNaN(chapter)) {
+            // bot.sendMessage(msg.from.id, text, { replyToMessage: msg.message_id });
+            const file = await downloadAudio(chapter)
+            let compressed
+            if (full) {
+                compressed = file
+            } else {
+                compressed = await compressAudio(chapter)
+            }
+            console.log(file, 'DOWNLOADED')
+            //TODO: Save stats
+            bot.sendAudio(msg.from.id, compressed, { replyToMessage: msg.message_id, title: `Episodio ${chapter} - El enjambre` })
+        } else {
+            bot.sendMessage(msg.from.id, 'Debe escribir solo el número del episodio.', { replyToMessage: msg.message_id });
+        }
 
     } catch (err) {
         bot.sendMessage(msg.from.id, 'Ha ocurrido un error procesando el episodio.', { replyToMessage: msg.message_id });
     }
-
 }
-bot.on(['/start'], (msg) => msg.reply.text('Para obetener una versión comprimida de un episodio, escriba /episodio [NUMERO].\nPor ejemplo: /episodio 3'));
-bot.on(/^\/episodio (.+)$/, findchapter);
 
+const findchapter = async (msg, props) => {
+    getAudio(msg, props, false)
+}
+
+const findchapterfull = async (msg, props) => {
+    getAudio(msg, props, true)
+}
+
+const getstats = async (msg) => {
+    const stats = db.get('podcasts')
+    const salida = stats.map(el=> `${el.name} - ${el.countcomp} comprimidas - ${el.countfull} calidad`).join('\n')
+    bot.sendMessage(msg.from.id, `Estadísticas de descargas: \n${salida}`, { replyToMessage: msg.message_id })
+}
+
+bot.on(['/start', '/ayuda'], (msg) => {
+    msg.reply.text('Para obtener la versión comprimida de un episodio, escriba /comprimido [NÚMERO].\nPor ejemplo: /comprimido 3 \n');
+    msg.reply.text('Si deseas escuchar el episodio en buena calidad, escriba /episodio [NÚMERO].\nPor ejemplo: /episodio 3 \n');
+    msg.reply.text('Para ver las estadísticas de descargas, escriba /estadisticas');
+
+})
+bot.on(/^\/comprimido (.+)$/, findchapter);
+bot.on(/^\/episodio (.+)$/, findchapterfull);
+bot.on(['/estadisticas'], getstats);
 bot.start();
